f4965e5efa5c0e67c0e72276a3cca314aef5d620
muresan-gabriel <gabiimuresan23@gmail.com>
2022-04-27 22:16:27 +0300

    Update of homework 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage.js";
+
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <div className="container-fluid">
+          <Routes>
+            <Route path="/" element={<HomePage />} exact />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/HomePage.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div className="container-fluid d-flex justify-content-center align-items-center flex-column navigation-menu">
+      <h1>La Moară - Cognizant Softvision Course</h1>
+      <span className="personal">
+        By&nbsp;
+        <a
+          className="personal personal-link"
+          href="https://gabrielmuresan.site"
+          target="_blank"
+        >
+          Gabriel Muresan
+        </a>
+      </span>
+      <h2 className="homework-heading">Homework navigation menu</h2>
+      <a
+        className="personal personal-link"
+        href="../../old-first-course/homework_1/index.html"
+      >
+        Homework #1
+      </a>
+      <a className="personal personal-link" href="#">
+        Homework #2 (inactive)
+      </a>
+      <a className="personal personal-link" href="#">
+        Homework #3 (inactive)
+      </a>
+      <a className="personal personal-link" href="#">
+        Homework #4 (inactive)
+      </a>
+      <a className="personal personal-link" href="#">
+        Homework #5 (inactive)
+      </a>
+      <a className="personal personal-link" href="#">
+        Homework #6 (inactive)
+      </a>
+      <a className="personal personal-link" href="#">
+        Homework #7 (inactive)
+      </a>
+      <a className="personal personal-link" href="#">
+        Homework #8 (inactive)
+      </a>
+    </div>
+  );
+};
+
+export default HomePage;
