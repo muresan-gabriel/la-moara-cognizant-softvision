cdec94a385330a7910970b7e1cd6c36f55de767a
muresan-gabriel <gabiimuresan23@gmail.com>
2022-05-04 18:56:26 +0300

    Second problem, no formatting

--- public/homework_2/script.js
+++ public/homework_2/script.js
@@ -1,5 +1,13 @@
 "use strict";
 
+// Useful functions
+
+const isPrime = (number) => {
+  for (let i = 2, s = Math.sqrt(number); i <= s; i++)
+    if (number % i === 0) return false;
+  return number > 1;
+};
+
 // 1. Create a function which takes as a parameter a number and
 // returns it's double if the number is odd, otherwise,
 // if the number is even, it returns the number substracted by 2.
@@ -16,3 +24,19 @@ function isEvenOrOdd(number) {
 }
 
 console.log(isEvenOrOdd(5));
+
+// 2. Create a function which stores in an array only the prime numbers
+// from another array received as a parameter. Loop through the array using a
+// 'for' loop. Log the result in the console.
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
+
+const savePrimes = (arr) => {
+  let arrOfPrimes = [];
+  arr.forEach((element) => {
+    if (isPrime(element)) arrOfPrimes.push(element);
+  });
+  return arrOfPrimes;
+};
+
+console.log(savePrimes(arr));
